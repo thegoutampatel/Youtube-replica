bf629dd7063a5cb9de1a70b8811d16dfadbef2b8
thegoutampatel <goutampatel9131@gmail.com>
2024-04-01 12:31:17 +0530

    Fix: App Crashed fixing this

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("@tailwindcss/line-clamp")],
+};
